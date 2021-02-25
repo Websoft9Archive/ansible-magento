003f9a8b9789460d5c380303c71d88e20dee2955
zengxc <zxc916684@163.com>
2021-02-25 10:56:54 +0800

    remove magento upgrade

--- roles/magento/templates/magento_install_sample.sh
+++ roles/magento/templates/magento_install_sample.sh
@@ -3,4 +3,5 @@
 sleep 1  
 echo "{{magento_repo_password}}"   
 sleep 1  
-echo "y")|/data/wwwroot/magento/bin/magento sampledata:deploy -n
+echo "y"
+sleep 1)|/data/wwwroot/magento/bin/magento sampledata:deploy -n
